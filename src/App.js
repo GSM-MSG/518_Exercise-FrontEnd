fd5102b4cf57ab72f4659aa80b3d598883fbe974
송현우 <shw9052@naver.com>
2023-05-01 08:09:52 +0900

    finish project

--- src/App.js
+++ src/App.js
@@ -10,23 +10,14 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/story" element={<Story />} />
-      </Routes>
-=======
       <AnimatePresence>
         <Routes location={location} key={location.pathname}>
           <Route path="/" exact element={<Home />} />
           <Route path="/story" element={<Story />} />
           <Route path="/quiz" element={<Quiz />} />
-          <Route path="/moment" element={<Moment />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </AnimatePresence>
->>>>>>> 5b47a4600dccac9433f4b83f915672e0409c44fb
     </div>
   );
 }
